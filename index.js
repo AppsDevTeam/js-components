379eb4bbe96b55170579a9155bb041827258fde2
Tomáš Kudělka <thorewi@gmail.com>
2021-03-18 23:46:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import ComponentLoader from './src/ComponentLoader';
 
 export default ComponentLoader;
+
+$.fn.adtAjax = function (e, options) {
+	return $.nette.ajax(options || {}, this[0], e);
+};
