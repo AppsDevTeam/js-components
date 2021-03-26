ff8d89dd1b4afffc40c5b792b9fcdfe0c2eedb20
Viktor Masicek <viktor@appsdevteam.com>
2021-03-26 13:52:28 +0100

    InputClear - check the selector is set

--- src/InputClear/index.js
+++ src/InputClear/index.js
@@ -1,8 +1,14 @@
 function run(options) {
 	$.nette.ext('live').after(function($el) {
 		$el.find('[data-adt-input-clear]').each(function() {
-			const options = $(this).data('adt-input-clear');
+			const options = $(this).data('adt-input-clear') || {};
 			const selector = options.selector;
+
+			if (!selector) {
+				console.error('You have to set the option.selector in JS component InputClear.');
+				return;
+			}
+
 			const confirmMessage = options.confirmMessage;
 
 			$(this).click(function () {
