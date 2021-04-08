512446905912dceea41d68a4fcbfbb32e20ebd12
Korca <tomaskorcina@seznam.cz>
2021-04-08 12:02:58 +0200

    f-select2-languages
    pridani nastaveni cestiny do select2 a ajaxSelectu ... pro jine jazyky nutnost naimportovat pozadovany js file ze select2

--- src/Select2/index.js
+++ src/Select2/index.js
@@ -1,19 +1,15 @@
 import 'select2';
 import 'select2/dist/css/select2.min.css';
 import '@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.min.css'
+import 'select2/dist/js/i18n/cs'
 
 function run(options) {
 	$.nette.ext('live').after(function($el) {
 
 		$el.find('[data-adt-select2]').each(function () {
-			var selectOptions = $(this).data('adt-select2') || {};
-			var minimumSearchResults = (selectOptions.minimumResultsForSearch !== undefined) ? selectOptions.minimumResultsForSearch : 0;
-
-			$(this).select2({
-				theme: 'bootstrap4',
-				minimumResultsForSearch: minimumSearchResults,
-			});
-		})
+			// musí zde být defaultní jazyk "en", pokud zde nebyl a byla importována čeština.. tak se i bez poslání json params nastavila čeština
+			$(this).select2($.extend({theme: 'bootstrap4', language: "en"}, $(this).data('adt-select2') || {}));
+		});
 	});
 }
 
