9958b8f56ef5914005ab89f0ca8a94d1b12011f3
Mirda <>
2021-08-11 13:41:39 +0200

    bs-5 support

--- src/Select2/index.js
+++ src/Select2/index.js
@@ -1,6 +1,6 @@
 import 'select2';
-import 'select2/dist/css/select2.min.css';
-import '@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.min.css'
+import 'select2/src/scss/core';
+import 'select2-bootstrap-5-theme/src/include-all'
 import 'select2/dist/js/i18n/cs'
 
 function run(options) {
@@ -8,7 +8,7 @@ function run(options) {
 
 		$el.find('[data-adt-select2]').each(function () {
 			// musí zde být defaultní jazyk "en", pokud zde nebyl a byla importována čeština.. tak se i bez poslání json params nastavila čeština
-			$(this).select2($.extend({theme: 'bootstrap4', language: "en"}, $(this).data('adt-select2') || {}));
+			$(this).select2($.extend({theme: 'bootstrap-5', language: "en"}, $(this).data('adt-select2') || {}));
 		});
 	});
 }
