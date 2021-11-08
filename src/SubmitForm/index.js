d80e98d5ffb4833bf54e54cf905a3f5c596a6cfd
Viktor Mašíček <viktor@masicek.net>
2021-11-08 15:10:46 +0100

    Apply only on form with specific attribute

--- src/SubmitForm/index.js
+++ src/SubmitForm/index.js
@@ -3,7 +3,7 @@ const Scrollparent = require("scrollparent");
 function run(options) {
 	$.nette.ext("submitForm", {
 		before: function (xhr, settings) {
-			if (settings.nette && settings.nette.form) {
+			if (settings.nette && settings.nette.form && settings.nette.form.attr('data-adt-submit-form') != undefined) {
 				settings.nette.form.find('button').prop('disabled', true);
 				settings.nette.el.find('.js-spinner').removeClass('d-none');
 				settings.nette.form.find('.js-error').remove();
@@ -12,13 +12,13 @@ function run(options) {
 		},
 		success: function (payload, status, xhr, settings) {
 			// if there is no redirect, we will enable buttons
-			if (!payload.redirect && settings.nette && settings.nette.form) {
+			if (!payload.redirect && settings.nette && settings.nette.form && settings.nette.form.attr('data-adt-submit-form') != undefined) {
 				settings.nette.el.find('.js-spinner').addClass('d-none');
 				settings.nette.form.find('button').prop('disabled', false);
 			}
 		},
 		error: function (xhr, status, error, settings) {
-			if (settings.nette && settings.nette.form) {
+			if (settings.nette && settings.nette.form && settings.nette.form.attr('data-adt-submit-form') != undefined) {
 				settings.nette.form.find('.js-errors').append('<div class="alert alert-danger js-error">' + xhr.responseJSON['error'] + '</div>');
 
 				settings.nette.el.find('.js-spinner').addClass('d-none');
@@ -27,7 +27,7 @@ function run(options) {
 		},
 		complete:  function (xhr, status, settings) {
 			// if there are errors we will scroll to first of them
-			if (settings.nette && settings.nette.form && settings.nette.form.find('.js-error').length > 0) {
+			if (settings.nette && settings.nette.form && settings.nette.form.attr('data-adt-submit-form') != undefined && settings.nette.form.find('.js-error').length > 0) {
 				const $error = settings.nette.form.find('.js-error:first');
 
 				// we have to find first scrollable element (it can be document or modal for example)
