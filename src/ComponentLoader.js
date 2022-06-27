48792be66411c9230fb66d31d6fdc8b2f0ec850c
Viktor Mašíček <viktor@masicek.net>
2022-06-27 15:41:15 +0200

    Do not want to require an import path prefix

--- src/ComponentLoader.js
+++ src/ComponentLoader.js
@@ -20,7 +20,7 @@ const init = (selector, path) => {
 		loadedComponents.push(path);
 
 		if (path.includes('/')) {
-			import('app/' + path + '/index.js').then(component => {
+			import(path + '/index.js').then(component => {
 				component.default.run(componentsConfig[selector] || {});
 			});
 
