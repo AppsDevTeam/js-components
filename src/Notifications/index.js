584bd16aa1eb3992e5fe2591721b8ead3f23a603
Pavel Konvička <pavel.konvicka@gmail.com>
2026-04-07 23:33:51 +0200

    add remove firebase

--- src/Notifications/index.js
+++ src/Notifications/index.js
@@ -1,7 +1,21 @@
-import { getToken } from "firebase/messaging";
+import { getToken, deleteToken } from "firebase/messaging";
 
 const run = (config) => {
-    $('[data-adt-notifications]').on('click', function () {
+    const $enableBtn = $('[data-adt-notifications-enable]');
+    const $disableBtn = $('[data-adt-notifications-disable]');
+
+    const updateButtons = () => {
+        if (Notification.permission === 'granted') {
+            $enableBtn.hide();
+            $disableBtn.show();
+        } else {
+            $enableBtn.show();
+            $disableBtn.hide();
+        }
+    };
+
+    // Enable notifications
+    $enableBtn.on('click', function () {
         if (window.messaging) {
             Notification.requestPermission().then(function (permission) {
                 if (permission !== 'granted') {
@@ -12,9 +26,11 @@ const run = (config) => {
                 getToken(window.messaging, { vapidKey: config.vapidKey })
                     .then(function (currentToken) {
                         if (currentToken) {
+                            window.adtNotificationsToken = currentToken;
                             $.nette.ajax({
                                 url: config.setFirebaseTokenLink.replace('__firebaseToken__', currentToken)
                             });
+                            updateButtons();
                         } else {
                             alert(_('appJs.firebase.error.notificationsPermissionError'));
                         }
@@ -28,6 +44,50 @@ const run = (config) => {
             alert(_('appJs.firebase.error.notificationsNotSupported'));
         }
     });
+
+    // Disable notifications
+    $disableBtn.on('click', function () {
+        if (window.messaging) {
+            deleteToken(window.messaging)
+                .then(function () {
+                    // Unregister service worker
+                    return navigator.serviceWorker.getRegistrations();
+                })
+                .then(function (registrations) {
+                    registrations.forEach(function (registration) {
+                        if (registration.active && registration.active.scriptURL.includes('firebase-messaging-sw')) {
+                            registration.unregister();
+                        }
+                    });
+                })
+                .catch(function (err) {
+                    console.error(err);
+                });
+        }
+
+        $.nette.ajax({
+            url: config.removeAllFirebaseTokensLink
+        });
+
+        window.adtNotificationsToken = null;
+        updateButtons();
+    });
+
+    // Initial state: check if notifications are already enabled
+    if (Notification.permission === 'granted' && window.messaging) {
+        getToken(window.messaging, { vapidKey: config.vapidKey })
+            .then(function (currentToken) {
+                if (currentToken) {
+                    window.adtNotificationsToken = currentToken;
+                }
+                updateButtons();
+            })
+            .catch(function () {
+                updateButtons();
+            });
+    } else {
+        updateButtons();
+    }
 }
 
 export default { run };
